8c6d7738b805d8f97851774efeecf5e833fbf269
Bargan Sergei <bargansergei333#gmail.com>
2025-11-17 18:52:44 +0300

    i found bag in index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,17 +19,14 @@ timeDelta.addEventListener('input', (event: Event & { target: HTMLInputElement }
     output.value = String(value)
 })
 
+// Task 2
+
 const form = document.querySelector('.formTask2') as HTMLFormElement
 const inputForm = form.querySelector('.formTask2-input') as HTMLInputElement
 const outputForm = form.querySelector('.formOutput') as HTMLOutputElement
 
 form.addEventListener('submit', (ev) => {
-    console.log('dasda')
     ev.preventDefault()
-    for (const element of inputForm.value) {
-        if (element !== ')' && element !== '(') {
-        }
-    }
     if (checkSequence(inputForm.value)) {
         outputForm.value = 'Верная последовательность'
     } else {
